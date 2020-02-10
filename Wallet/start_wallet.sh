e736cad241e34d51c6cbe5ac43894c260b1cb0b1
bublik77 <andrii.bobrovych@gmail.com>
2020-02-10 15:59:06 +0200

    Update start_wallet.sh

--- Wallet/start_wallet.sh
+++ Wallet/start_wallet.sh
@@ -10,11 +10,11 @@
 #
 ###############################################################################
 
-NODEOSBINDIR="/opt/bin"
+NODEOSBINDIR="/opt/bin/bin"
 DATADIR="/opt/Jungle2Testnet/Wallet"
 WALLET_HOST="127.0.0.1"
 WALLET_POSRT="3000"
 
 
 $DATADIR/stop_wallet.sh
-$NODEOSBINDIR/keosd --config-dir $DATADIR --wallet-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
+$NODEOSBINDIR/keosd --config-dir $DATADIR --wallet-dir $DATADIR --unix-socket-path $DATADIR/keosd.sock --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
